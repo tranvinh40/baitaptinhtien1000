4e92b47e657475828605a3a98f7d9f2081ce4e99
Trần  Vinh <117698002+tranvinh40@users.noreply.github.com>
2022-11-22 10:16:57 +0700

    Create baotap.c

---
+++ baotap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define t 500
+int main () {
+	// d la ngay 20-11 nha sach giam 20%
+	// c la tong tien 10 cuon so tay
+	// gia tien moi cuon so la x
+	int c, d, x;
+	c = t * 10;
+	
+	d = c * 20 / 100;
+	
+	printf ("tong tien 10 cuon so tay la %d", d);
+	
+}
